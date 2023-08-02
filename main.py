9d496433ccbb761e41596aeb7df63b7aa5989741
JiSoo Park <jpark@aba.com>
2023-08-02 03:38:30 -0400

    First page trial

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import pygame
+import shelve
 from sys import exit
+
+# Shelve save and load data
+SAVE_DATA = shelve.open("Save User Data")
+
+# Start menu has two buttons: New user, Returned users
+# new user: Let user input their name & press the level test button => name is saved and user is lead level test screen
+# old user: let user input their name and submit => Welcome the user with the best score and let them re-do the game. 
+# if the new score is higher than the old score, replace the old score with new best score
+
+slategrey = (112, 128, 144)
+lightgrey = (165, 175, 185)
+blackish = (10, 10, 10)
+
+# Function to create a button
+def create_button(x, y, width, height, hovercolor, defaultcolor):
+    mouse = pygame.mouse.get_pos()
+    # Mouse get pressed can run without an integer, but needs a 3 or 5 to indicate how many buttons
+    click = pygame.mouse.get_pressed(3)
+    if x + width > mouse[0] > x and y + height > mouse[1] > y:
+        pygame.draw.rect(screen, hovercolor, (x, y, width, height))
+        if click[0] == 1:
+            return True
+    else:
+        pygame.draw.rect(screen, defaultcolor, (x, y, width, height))
 
 def display_score():
     
@@ -9,10 +34,46 @@ def display_score():
     screen.blit(score_surf, score_rect)
     return current_time
 
+def start_menu():
+    start_text = test_font.render("WELCOME TO RPG 2D ACADEMIC GAME", True, (64, 64, 64))
+    start_rect = start_text.get_rect(center = (400, 50))
+    new_text = test_font.render("New User", True, blackish)
+    old_text = test_font.render("Load User", True, blackish)
+
+    while True:
+        screen.fill((94, 129, 162))
+        screen.blit(start_text, start_rect)
+        newUserButton = create_button((screen_width / 2) - 100, int(screen_height * .33), 200, 50, lightgrey, slategrey)
+
+        if newUserButton:
+            new_game()
+        screen.blit(new_text, ((screen_width / 2) - (new_text.get_width() / 2), int(screen_height * .33)))
+
+        oldUserButtton = create_button((screen_width / 2) - 100, screen_height / 2, 200, 50, lightgrey, slategrey)
+
+        if oldUserButtton:
+            load_screen()
+
+        # Load CEO button text
+        screen.blit(old_text, ((screen_width / 2) - (new_text.get_width() / 2), screen_height / 2))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        clock.tick(15)
+        
+#def new_game():
+
+
 # Initiates all the function related to pygame
 pygame.init()
 # Display surface of the screen (width, height)
-screen = pygame.display.set_mode((800, 400))
+screen_width = 800
+screen_height = 400
+screen = pygame.display.set_mode((screen_width, screen_height))
 # Update title of the surface
 pygame.display.set_caption('RPG 2D Academic Game')
 # Control frame break
@@ -42,7 +103,14 @@ snoopy_x_pos = 0
 snoopy_rect = snoopy_surf.get_rect(midbottom = (50, 280))
 snoopy_grav = 0
 
-#Intro Screen
+
+#New game screen for new user
+
+
+#Load game screen for existing user
+
+
+#Redo Screen
 snoopy_stand = pygame.image.load('graphic/snoopy_stand.png').convert_alpha()
 snoopy_stand_rect = snoopy_stand.get_rect(center = (400, 200))
 
@@ -58,6 +126,7 @@ pygame.time.set_timer(obstacle_timer, 900)
 
 # This game will run forever in this loop
 while True:
+    start_menu()
     for event in pygame.event.get():
         # pygame.QUIT is x button
         if event.type == pygame.QUIT:
@@ -96,7 +165,7 @@ while True:
             cat_rect.left = 800
         screen.blit(cat_surf,cat_rect)
 
-        
+        # Snoopy moving forward
         # snoopy_rect.left += 1
 
         # Player
