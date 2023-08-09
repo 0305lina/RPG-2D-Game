7700e91dc71b9d540aef6254827615c086fa947f
JiSoo Park <jip223@lehigh.edu>
2023-08-09 08:22:55 -0400

    Finish Intro Page

--- login.py
+++ login.py
@@ -14,7 +14,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Space Game")
 
 # Initialize constants
-font = pygame.font.SysFont("arial", 25)
+font = pygame.font.SysFont("arial", 40)
 smallfont = pygame.font.SysFont("arial", 15)
 slategrey = (112, 128, 144)
 lightgrey = (165, 175, 185)
@@ -31,29 +31,28 @@ def create_button(msg, x, y, width, height, hovercolor, defaultcolor):
     if x + width > mouse[0] > x and y + height > mouse[1] > y:
         pygame.draw.rect(screen, hovercolor, (x, y, width, height))
         if click[0] == 1:
-            first_level()
+            login()
     else:
         pygame.draw.rect(screen,defaultcolor, (x, y, width, height))
     # Start button text
-    startbuttontext = smallfont.render(msg, True, blackish)
-    screen.blit(startbuttontext, (int(890 + (width / 2)), int(y + (y / 2))))
+    startbuttontext = font.render(msg, True, blackish)
+    screen.blit(startbuttontext, (int(150), int(470)))
     
 # Start menu returns true until we click the start button
 def start_menu():
-    startText = font.render("The Space Game", True, slategrey)
+    startText = font.render("Space Game", True, (slategrey))
     
     while True:
         screen.fill((0, 0, 0))
-        screen.blit(startText, ((screen_width - startText.get_width()) / 2, 0))
-        
-        # start button (left, top, width, height)
-        create_button("Start!", screen_width - 130, 7, 125, 26, lightgrey, slategrey)
         
         # Displays the board room picture
-        screen.blit(space, (0, 40))
+        screen.blit(pygame.transform.scale(space, (550,700)), (0,0))
+        screen.blit(startText, ((screen_width - startText.get_width()) / 2, 20))
+        # start button (left, top, width, height)
+        create_button("LOGIN", 100, 450, 200, 80, lightgrey, slategrey)
         
         for event in pygame.event.get():
-            if event.type == pygame.QUIT():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
                 
@@ -61,8 +60,8 @@ def start_menu():
         clock.tick(60)
         return True
     
-def first_level():
-    startText = font.render("The Space Game", True, slategrey)
+def login():
+    startText = font.render("WELCOME", True, slategrey)
     
     while True:
         screen.fill((0, 0, 0))
@@ -80,7 +79,7 @@ def first_level():
 # Game Loop
 while True:
     
-    start_menu
+    start_menu()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
