ad674b298f59c85b134a2a983d6a6125fd1ff997
JiSoo Park <jip223@lehigh.edu>
2023-08-09 02:23:42 -0400

    Login Page Error

--- login.py
+++ login.py
@@ -21,7 +21,8 @@ lightgrey = (165, 175, 185)
 blackish = (10, 10, 10)
 
 # Load Images
-Space = pygame.image.load("space_start.jpg")
+space = pygame.image.load("space_start.jpg")
+
 
 # Function to create a button
 def create_button(msg, x, y, width, height, hovercolor, defaultcolor):
@@ -40,4 +41,51 @@ def create_button(msg, x, y, width, height, hovercolor, defaultcolor):
 # Start menu returns true until we click the start button
 def start_menu():
     startText = font.render("The Space Game", True, slategrey)
+    
+    while True:
+        screen.fill((0, 0, 0))
+        screen.blit(startText, ((screen_width - startText.get_width()) / 2, 0))
+        
+        # start button (left, top, width, height)
+        create_button("Start!", screen_width - 130, 7, 125, 26, lightgrey, slategrey)
+        
+        # Displays the board room picture
+        screen.blit(space, (0, 40))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                pygame.quit()
+                sys.exit()
+                
+        pygame.display.update()
+        clock.tick(60)
+        return True
+    
+def first_level():
+    startText = font.render("The Space Game", True, slategrey)
+    
+    while True:
+        screen.fill((0, 0, 0))
+        screen.blit(startText, ((screen_width - startText.get_width()) / 2, 0))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+        pygame.display.update()
+        clock.tick(60)
+        
+
+# Game Loop
+while True:
+    
+    start_menu
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(60)
     
